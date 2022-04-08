d8c64414aecc95ef2b9affb69b7054966afcdf8d
quentingrchr <51860690+quentingrchr@users.noreply.github.com>
2022-04-08 17:22:29 +0200

    update aliases

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
@@ -6,4 +8,25 @@ module.exports = {
     "@storybook/preset-scss",
   ],
   framework: "@storybook/react",
+  webpackFinal: async (config) => {
+
+    
+    const customAliases = {
+        "@components": path.resolve(__dirname, '../src/components/'),
+        "@styles":     path.resolve(__dirname, '../src/styles/'),
+        "@public":     path.resolve(__dirname, '../public/'),
+        "@interfaces": path.resolve(__dirname, '../src/interfaces/'),
+        "@types":      path.resolve(__dirname, '../src/types/')
+        // add future aliases here (see tsconfig.json)
+    }
+
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      ...customAliases,
+    };
+    
+   
+
+    return config;
+  },
 };
