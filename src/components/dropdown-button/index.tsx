368012eff995955ae6e235d27e812865f8020fa8
quentingrchr <51860690+quentingrchr@users.noreply.github.com>
2022-05-07 11:16:31 +0200

    add drowdown button

--- src/components/dropdown-button/index.tsx
+++ src/components/dropdown-button/index.tsx
@@ -1,12 +1,20 @@
 import React, { useState, useRef } from "react"
 import cn from "classnames"
 import s from "./index.module.scss"
-import { Button } from "@components/button"
-export type IProps = {
+import Link from "next/link"
 
+interface Choice { 
+    label: string
+    onClick?: () => void
+    to?: string
+}
+export interface IProps {
+    className?: string
+    label: string
+    choices: Choice[]
 }
 
-export const DropdownButton: React.FC<IProps> = (props: IProps) => {
+export const DropdownButton: React.FC<IProps> = ({label = 'Label', choices = [{label: 'Choice', onClick: () => {}}]}: IProps) => {
     const [isOpen, setIsOpen] = useState(false)
     const dropdownRef = React.useRef<HTMLDivElement>(null)
 
@@ -36,16 +44,36 @@ export const DropdownButton: React.FC<IProps> = (props: IProps) => {
             if (isDescendant(dropdownRef.current, e.relatedTarget) || dropdownRef.current === e.relatedTarget) return
             setIsOpen(false)
         }
-    }   
+    }  
+
 
 
     return (
         <div className={cn(s.container, {[s.isOpen] : isOpen})}>
-            <button className={s.button} onClick={handleClick} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>test</button>
+            <button className={s.button} onClick={handleClick} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>{label}</button>
             <div ref={dropdownRef} className={s.dropdown} onMouseLeave={handleMouseLeave}>
                 <ul className={cn(s.list, {[s.isOpen] : isOpen})}>
-                    <li className={s.item}> Choice 1 </li>
-                    <li className={s.item}> Choice 2 </li>
+                    {
+                        choices.map((choice, index) => {
+                            if (choice.to) {
+                                return (
+                                    <li key={index} className={s.item}>
+                                        <Link href={choice.to}>{choice.label}</Link>
+                                    </li>
+                                )
+                            } else if (choice.onClick) { 
+                                return (
+                                    <li key={index} className={s.item}>
+                                        <button onClick={() => {
+                                            setIsOpen(false)
+                                            if(choice.onClick) choice.onClick()
+                                        }}>
+                                            {choice.label}</button>
+                                    </li>
+                                )
+                            }
+                        })
+                    }
                 </ul>
             </div>
         </div>
