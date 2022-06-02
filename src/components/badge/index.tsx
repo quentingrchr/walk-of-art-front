9cea8b6c2c8dd182840612dbe475ea59a72db55c
quentingrchr <51860690+quentingrchr@users.noreply.github.com>
2022-06-02 23:00:48 +0200

    wip

--- src/components/badge/index.tsx
+++ src/components/badge/index.tsx
@@ -7,7 +7,7 @@ export type IProps = {
     type: BadgeTypes
 }
 
-export const Badge: React.FC<IProps> = ({ type = "success" }: IProps) => {
+export const Badge: React.FC<IProps> = ({ type = "completed" }: IProps) => {
     const label = type === "completed" ? "Completed" : "In progress"
     return (
         <div className={cn(s.container, s[type])} title={label}>
