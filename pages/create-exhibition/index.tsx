85f6c534b7b5de4a7820e2df44ea447f33813b0f
Reda Hamouche <reda.hamouche@outlook.com>
2022-06-23 17:10:54 +0200

    passe d alexanne

--- pages/create-exhibition/index.tsx
+++ pages/create-exhibition/index.tsx
@@ -1,11 +1,51 @@
 import React from "react";
-import { TemplatePage, Text } from "@components"
+import { TemplatePage, Text, HeadingStrong, Stepper, Link  } from "@components"
+import { IStep } from "../../src/components/stepper"
+import styles from './index.module.scss'
 
+const STEPS = [
+    {
+        id: 1,
+        label: "Step 1",
+        number: 1,
+        completed: false,
+    },
+    {
+        id: 2,
+        label: "Step 2",
+        number: 2,
+        completed: false,
+    },
+    {
+        id: 3,
+        label: "Step 3",
+        number: 3,
+        completed: false,
+    },
+    
+]
 const CreateExhibition: React.FC = () => {
+    const [activeStep, setActiveStep] = React.useState<number>(0)
+    const [steps, setSteps] = React.useState<IStep[]>(STEPS)
+    const completeStep = (index: number) => {
+        setSteps(steps.map((step, i) => {
+            if (i === index) {
+                return {
+                    ...step,
+                    completed: true,
+                }
+            }
+            return step
+        }))
+    }
 return (
     <TemplatePage isLogged={true}>
         {/* PAGE CONTENT */}
-        <Text tag="h1" typo="paragraph-md">Page de création d'une exposition</Text>
+         <Link to='/' label='Retour à la connexion' classname={styles.link2}/>
+        <div className={styles.test2}>
+            <HeadingStrong elementColor="success" color="black" content="Création d'une oeuvre" size="md" />
+            <Stepper activeStep={activeStep} steps={steps} completeOne={completeStep}/> 
+        </div>
     </TemplatePage>
 )
 }
