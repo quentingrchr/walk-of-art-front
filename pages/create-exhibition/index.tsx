d0040a1e959f9e74a485dcca73de4eb051e46f39
Alexanne Brunold <alexanne.brunold@hetic.net>
2022-06-23 18:27:49 +0200

    ADD Inputs and artwork component

--- pages/create-exhibition/index.tsx
+++ pages/create-exhibition/index.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import { TemplatePage, HeadingStrong, ExhibitionStepper, Link, Input, Artwork  } from "@components"
 import { IStep } from "../../src/components/stepper"
@@ -42,27 +41,12 @@ const CreateExhibition: React.FC = () => {
 return (
     <TemplatePage isLogged={true}>
         {/* PAGE CONTENT */}
-<<<<<<< HEAD
          <Link to='/' label='Retour à la connexion' classname={styles.link}/>
          <div className={styles.background}></div>
         <div className={styles.heading}>
             <HeadingStrong elementColor="success" color="black" content="Création d'une oeuvre" size="md" /> 
 
             {/* <Artwork src={Image} alt={"test"} size={"small"}/>  */}
-=======
-         <Link to='/' label='Retour à la connexion' classname={styles.link2}/>
-        <div className={styles.test2}>
-            <HeadingStrong elementColor="success" color="black" content="Création d'une oeuvre" size="md" />
-            <Stepper activeStep={activeStep} steps={steps} completeOne={completeStep}/>
-
-<Artwork src={image} alt={"test"}/>
-
-            <label htmlFor="exhibitionTitle">Titre de l'exposition</label>
-            <Input type='text' placeholder='Ex : La méduse sur le radeau' id={"exhibitionTitle"} value={"Titre"} register={undefined}/>
-
-            <label htmlFor="exhibitionDescription">Descriptif de l'exposition</label>
-            <Input type='text' placeholder='Deescription de mon exposition' id={"exhibitionDescription"} value={"Titre"} register={undefined}/> 
->>>>>>> 16179ad (ADD Inputs and artwork component)
         </div>
             <ExhibitionStepper activeStep={activeStep} steps={steps} completeOne={completeStep} setActiveStep={setActiveStep}/>
     </TemplatePage>
---
+++ src/components/form/input/index.tsx
@@ -0,0 +1,48 @@
+import React, { useRef } from "react"
+import styles from "./index.module.scss"
+import  cx  from "classnames";
+import { Icon } from "@components"
+import { InputTypes } from "../../../types";
+
+export type IProps = {
+    id: string
+    placeholder: string
+    value: string
+    type?: InputTypes
+    register: any
+}
+
+
+const TextInput = ({ id, placeholder }: IProps) => { 
+    return (
+        <div className={styles.container}>
+            <input id={id} placeholder={placeholder} className={styles.input} type="text" />
+        </div>
+    )
+};
+
+const PasswordInput = ({ id, placeholder, register }: IProps) => { 
+    const [showPassword, setShowPassword] = React.useState(false)
+
+    return (
+        <div className={styles.container}>
+            <input id={id} placeholder={placeholder} className={styles.input} type={showPassword ? 'text' : 'password'} {...register(id)} />
+                <div className={cx('icon','cta')} onClick={() => setShowPassword(!showPassword)}>
+                    <Icon type="avatar" size="large" />
+                </div>
+        </div>
+    )
+};
+
+
+export const Input: React.FC<IProps> = ({ type, ...otherProps}: IProps) => {
+    switch (type) { 
+        case 'text':
+            return <TextInput {...otherProps} />
+        case 'password':
+            return <PasswordInput {...otherProps} />
+        default:
+            return <TextInput {...otherProps} />
+    }
+    
+}
