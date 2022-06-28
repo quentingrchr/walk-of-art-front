2e67d6d5197f5dee93652e8aeca1ed0ebe1308a5
Reda Hamouche <reda.hamouche@outlook.com>
2022-06-28 12:10:13 +0200

    added mediaSelector, steps, refacto some stuff

--- src/components/index.ts
+++ src/components/index.ts
@@ -32,3 +32,4 @@ export { NotificationWrapper } from "./notification-wrapper"
 export { EditProfile } from "./form/edit-profile"
 export { CreateWorkForm } from "./form/create-work/index";
 export { Artwork } from './artwork'
+export { MediaSelector } from './mediaSelector'
---
+++ src/components/mediaSelector/index.tsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react"
+import styles from "./index.module.scss"
+import cx from "classnames"
+import { Text } from "@components"
+import Link from "next/link"
+
+export type IProps = {
+}
+
+export const MediaSelector: React.FC<IProps> = (props: IProps) => {
+
+
+
+	return (
+   <div>hello media selector</div>
+	)
+}
