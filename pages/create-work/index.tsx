88aa199964ea3faa556bf81b0ca2d721626154c0
Reda Hamouche <reda.hamouche@outlook.com>
2022-06-28 15:14:49 +0200

    becareful createWork/exhi export probleme bros

---
+++ pages/create-work/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { TemplatePage, Text, CreateWorkForm, InputFile } from "@components";
+
+const CreateWork: React.FC = () => {
+  return (
+    <TemplatePage isLogged={true}>
+      {/* PAGE CONTENT */}
+      <Text tag="h1" typo="paragraph-md">
+        Page de création d'une oeuvre
+      </Text>
+    <CreateWorkForm />
+    </TemplatePage>
+  );
+};
+
+export default CreateWork;
