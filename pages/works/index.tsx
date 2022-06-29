0a60142c83dbad6d4d01a98a4e2bf92e06b6356d
Alexanne Brunold <alexanne.brunold@hetic.net>
2022-06-29 12:51:52 +0200

    ADD Better naming

--- pages/works/index.tsx
+++ pages/works/index.tsx
@@ -2,19 +2,22 @@ import React from "react";
 import { Icon, TemplatePage, Text, Search } from "@components"
 import { Checkbox } from "@components/checkbox";
 import style from './index.module.scss'
+import cn from "classnames"
 
 const Works: React.FC = () => {
     return (
         <TemplatePage isLogged={true}>
-            {/* PAGE CONTENT */}
             <Text tag="h1" typo="heading-md">Mes Oeuvres</Text>
-            <div className={style.a}>
-                <div className={style.x}>
+
+            <div className={style.researchContainer}>
+                <div className={style.checkboxesContainer}>
                     <Checkbox checkboxName={"Tous"} checkboxLabel={"Tous"} />
                     <Checkbox checkboxName={"Exposées"} checkboxLabel={"Exposées"} />
                     <Checkbox checkboxName={"Non exposées"} checkboxLabel={"Non exposées"} />
-                        <Icon type="downArrow" size={"small"}/>
-                        <p className={style.xx}>Date de création</p>
+                    <div className={style.dateChoiceContainer}>
+                    <Icon type="downArrow" size={"small"} />
+                        <p className={cn(style.textStyle, style.margin)}>Date de création</p>
+                    </div>
                 </div>
                
                 <Search id={""} placeholder={"Recherche oeuvre par le titre"}/>
