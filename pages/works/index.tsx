76ec2042caea8d3a3bf7bd707e602a50c215d1d9
quentintrouve <trouve.quentin@gmail.com>
2022-06-30 16:29:41 +0200

    feat/ continue jsx for filters and wait searchbar

--- pages/works/index.tsx
+++ pages/works/index.tsx
@@ -1,12 +1,45 @@
-import React from "react";
-import { TemplatePage, Text, HeadingStrong } from "@components"
+import React, { useState } from "react"
+import styles from "./index.module.scss"
+import cn from "classnames"
+import { TemplatePage, HeadingStrong, Checkbox, Icon, Text } from "@components"
+
 
 const Works: React.FC = () => {
+
+    const [filterExposed, setFilterExposed] = useState(false)
+    const [filterUnexposed, setFilterUnexposed] = useState(false)
+
+
     return (
         <TemplatePage isLogged={true}>
-            {/* PAGE CONTENT */}
-            <Text tag="h1" typo="paragraph-md">Page des Oeuvres</Text>
-            <HeadingStrong content="Mes oeuvres" elementColor="pink" size="md" />
+            <section className={styles.header}>
+                <HeadingStrong content="Mes oeuvres" elementColor="pink" size="md" />
+                <aside>
+                    <ul className={styles.filters}>
+                        <li className={styles.filter}>
+                            <Checkbox 
+                                checkboxLabel="Exposées"
+                                checkboxName="works-exposed"
+                                isChecked={filterExposed}
+                                onChange={() => setFilterExposed(prev => !prev)}
+                            />
+                        </li>
+                        <li className={styles.filter}>
+                            <Checkbox 
+                                checkboxLabel="Non exposées"
+                                checkboxName="works-unexposed"
+                                isChecked={filterUnexposed}
+                                onChange={() => setFilterUnexposed(prev => !prev)}
+                            />
+                        </li>
+                        <li className={cn(styles.filter, styles.filterDate)}>
+                            <Icon type="rightArrow" size="small" color="black" />
+                            <Text tag="p" typo="label">Date de création</Text>
+                        </li>
+                    </ul>
+                    <div></div>
+                </aside>
+            </section>
         </TemplatePage>
     )
 }
