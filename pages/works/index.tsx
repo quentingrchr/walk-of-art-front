740e91acdeda2611565a75ef036117f66b899517
quentintrouve <trouve.quentin@gmail.com>
2022-06-30 15:42:33 +0200

    feat/ start jsx for filters and wait searchbar

--- pages/works/index.tsx
+++ pages/works/index.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { TemplatePage, Text } from "@components"
+import { TemplatePage, Text, HeadingStrong } from "@components"
 
 const Works: React.FC = () => {
     return (
         <TemplatePage isLogged={true}>
             {/* PAGE CONTENT */}
             <Text tag="h1" typo="paragraph-md">Page des Oeuvres</Text>
+            <HeadingStrong content="Mes oeuvres" elementColor="pink" size="md" />
         </TemplatePage>
     )
 }
