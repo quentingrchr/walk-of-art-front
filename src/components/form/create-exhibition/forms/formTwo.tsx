ef85d53f0e24f38b03548d834e7ff3e6e221990b
Alexanne Brunold <alexanne.brunold@hetic.net>
2022-06-30 15:14:21 +0200

    ADD New step

--- src/components/form/create-exhibition/forms/formTwo.tsx
+++ src/components/form/create-exhibition/forms/formTwo.tsx
@@ -1,3 +1,4 @@
+
 import React, { useEffect } from "react";
 import styles from "./index.module.scss";
 import cn from "classnames";
@@ -16,61 +17,95 @@ export interface IRecapProps {
   handleBack: () => void;
   formState: any;
 }
-
 export const FormTwo: React.FC<IProps> = ({
     handleStepSubmit,
-    handleBack,
     defaultValues = {},
   }: IProps) => {
-    const { register, handleSubmit, watch } = useForm({
-      mode: "onBlur",
-      defaultValues,
-    });
-  
-    const watchPrimaryImage = watch("primary-image");
-    const watchSecondaryImages = watch([
-      "secondary-image-1",
-      "secondary-image-2",
-      "secondary-image-3",
-    ]);
+    const {
+      register,
+      handleSubmit,
+      formState: { errors },
+    } = useForm({ mode: "onBlur", defaultValues });
   
     const onSubmit = (e: any) => {
+      console.log("submit");
       e.preventDefault();
   
-      const requiredFieldIsAlreadyFilled = watch("primary-image").length > 0;
-  
-      if (requiredFieldIsAlreadyFilled) {
-        handleStepSubmit(watch());
-      } else {
-        handleSubmit((d) => {
-          handleStepSubmit(d);
-        })(e);
-      }
+      handleSubmit((d) => {
+        console.log(d);
+        handleStepSubmit(d);
+      })(e);
     };
   
     return (
       <form className={styles.formContainer} onSubmit={onSubmit}>
-        <InputFile
+        <InputGroup
+          placeholder="https://facebook.com/mon-profil"
           register={register}
-          primaryInput={{
-            name: "primary-image",
-            required: true,
-          }}
-          primaryValue={watchPrimaryImage || defaultValues.primaryImage}
-          secondaryInputs={[
-            { name: "secondary-image-1", required: false },
-            { name: "secondary-image-2", required: false },
-            { name: "secondary-image-3", required: false },
-          ]}
-          secondaryValues={watchSecondaryImages || defaultValues.secondaryImages}
+          required={true}
+          id="title"
+          type="text"
+          label="URL de mon profil Facebook"
+          guidance={
+            errors.title
+              ? {
+                  type: "error",
+                  message:
+                    "Le titre doit être rempli pour passer à l’étape suivante",
+                }
+              : null
+          }
         />
-        <div className={styles.ctaContainer}>
-          <Button
-            label={"Précédent"}
-            color="white"
-            bg="dark"
-            onClick={handleBack}
+        <InputGroup
+          placeholder="https://twitter.com/mon-profil"
+          register={register}
+          id="description"
+          type="text"
+          label="URL de mon profil Twitter"
+          guidance={null}
+        />
+        <InputGroup
+          placeholder="https://mon-site-personnel.com/"
+          register={register}
+          id="description"
+          type="text"
+          label="URL de mon site personnel"
+          guidance={null}
+        />
+          <InputGroup
+            placeholder="https://mon-portoflio.com/"
+            register={register}
+            id="description"
+            type="text"
+            label="URL de mon portfolio"
+            guidance={null}
           />
+        <InputGroup
+          placeholder="https://ma-boutique-en-ligne.com/"
+          register={register}
+          id="description"
+          type="text"
+          label="URL de ma boutique en ligne"
+          guidance={null}
+        />
+        <InputGroup
+          placeholder="https://ma-boutique-en-ligne.com/"
+          register={register}
+          id="description"
+          type="text"
+          label="URL de ma boutique en ligne"
+          guidance={null}
+        />
+        <InputGroup
+          placeholder="Texte avant le lien de donation"
+          register={register}
+          id="description"
+          type="text"
+          guidance={null}
+        />
+                  <Button label={"Ajouter un lien personnel"} color="white" bg="dark" type="submit" />
+
+        <div className={styles.ctaContainer}>
           <Button label={"Suivant"} color="white" bg="dark" type="submit" />
         </div>
       </form>
