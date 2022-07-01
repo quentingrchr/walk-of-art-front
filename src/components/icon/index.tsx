25de85e8f340fb2b6a3e1463f8ca1ffe960cde64
quentingrchr <51860690+quentingrchr@users.noreply.github.com>
2022-07-01 17:54:39 +0200

    désole j'ai tout cassé

--- src/components/icon/index.tsx
+++ src/components/icon/index.tsx
@@ -1,75 +1,66 @@
-import { Colors, Icons as IconsType , sizeIcon } from "@interfaces/index";
-import cn from "classnames";
-import styles from "./index.module.scss";
+import IcomoonReact from "icomoon-react"
+import iconSet from "@assets/fonts/icomoon/selection.json"
+import { Colors, Icons as IconsType, sizeIcon } from "@interfaces/index"
+import cn from "classnames"
+import styles from "./index.module.scss"
 
-import Avatar from "./avatar";
-import Notification from "./notification";
-import Logout from "./logout";
-import Profile from "./profile";
-import Commentary from "./commentary";
-import RightArrow from "./right-arrow";
-import Cross from "./cross";
-import Check from "./check";
-import Checkbox from "./checkbox";
-import Information from "./information";
-import DropFile from "./drop-file";
-import DownArrow from "./down-arrow";
-import SearchIcon from "./search-icon";
-import Chrono from "./chrono";
-import Calendar from "./calendar";
+// import Avatar from "./avatar"
+// import Notification from "./notification"
+// import Logout from "./logout"
+// import Profile from "./profile"
+// import Commentary from "./commentary"
+// import RightArrow from "./right-arrow"
+// import Cross from "./cross"
+// import Check from "./check"
+// import Checkbox from "./checkbox"
+// import Information from "./information"
+// import DropFile from "./drop-file"
+// import DownArrow from "./down-arrow"
+// import SearchIcon from "./search-icon"
+// import Chrono from "./chrono"
+// import Calendar from "./calendar"
 
 interface IProps {
-  type: IconsType;
-  size: sizeIcon;
-  color?: Colors | "none";
-  children?: React.ReactNode;
-  onClick?: (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
-}
-interface IIcons {
-  [String: string]: JSX.Element;
+  type: IconsType
+  size: sizeIcon
+  color?: Colors | "none"
+  children?: React.ReactNode
+  customSize?: number
+  onClick?: (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => void
+  position?: "inline" | "center"
 }
 
-const Icons: IIcons = {
-  avatar: <Avatar />,
-  notification: <Notification />,
-  logout: <Logout />,
-  profile: <Profile />,
-  commentary: <Commentary/>,
-  rightArrow: <RightArrow/>,
-  downArrow: <DownArrow />,
-  cross: <Cross/>,
-  check: <Check/>,
-  checkbox: <Checkbox/>,
-  information: <Information />,
-  searchIcon: <SearchIcon />,
-  "drop-file": <DropFile />,
-  chrono: <Chrono />,
-  calendar: <Calendar />,
-};
-
-const sizes: any = {
+const sizeMap: {
+  [key in sizeIcon]: string
+} = {
   xsmall: "0.8rem",
   small: "1.6rem",
   medium: "1.8rem",
   large: "2.2rem",
-  xlarge: "4rem", // not used yet
+  xlarge: "4rem",
   xxlarge: "8rem",
-};
+}
 
 export const Icon = ({
   type,
   onClick,
   size,
-  color = "none",
-  ...props
+  color = "black",
+  customSize,
+  position = "inline",
 }: IProps) => {
+  const computedSize = customSize !== undefined ? customSize : sizeMap[size]
   return (
     <div
       onClick={(e) => onClick !== undefined && onClick(e)}
-      className={cn(styles[color], "icon")}
-      style={{ width: sizes[size], height: sizes[size] }}
+      className={cn(styles[color], "icon", styles[position])}
     >
-      {Icons[type]}
+      <IcomoonReact
+        iconSet={iconSet}
+        icon={type}
+        size={computedSize}
+        className={cn(styles.icon, styles[type])}
+      />
     </div>
-  );
-};
+  )
+}
