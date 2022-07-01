af8b05d12dc503799125dfef24048b76498429bc
quentingrchr <51860690+quentingrchr@users.noreply.github.com>
2022-07-01 15:54:01 +0200

    wip

--- data/form.ts
+++ data/form.ts
@@ -1,61 +1,103 @@
-import * as yup from "yup";
-import { IFormInput } from "@components/form/login-form/interfaces";
+import * as yup from "yup"
+import { IFormInput } from "@components/form/login-form/interfaces"
 
-
-export const signInInputsSchema = yup.object().shape({
+export const signInInputsSchema = yup
+  .object()
+  .shape({
     email: yup.string().email().required(),
-    password: yup.string().min(8).required(),
-    passwordConfirmation: yup.string()
-    .oneOf([yup.ref('password'), null])
-}).required();
+    password: yup.string().min(2).required(),
+    passwordConfirmation: yup.string().oneOf([yup.ref("password"), null]),
+  })
+  .required()
 
 export const signInInputs: Array<IFormInput> = [
-        {
-            id: 'email',
-            type: "email",
-            label: "Email",
-            placeholder: "Entrez votre email",
-            guidance: (errors: any) =>  errors.email ?  ({ type: "error", message: "L'email n'est pas valide" })  : null
-        },
-        {
-            id: 'password',
-            type: "password",
-            label: "Mot de passe",
-            placeholder: "Entrez votre mot de passe",
-            guidance: (errors: any) =>  errors.password ?  ({ type: "error", message: "Le mot de passe doit contenir au moins 8 caractères" })  : null
-        },
-        /* todo add checkbox */
-] 
-    
+  {
+    id: "email",
+    type: "email",
+    label: "Email",
+    placeholder: "Entrez votre email",
+    guidance: (errors: any) =>
+      errors.email
+        ? { type: "error", message: "L'email n'est pas valide" }
+        : null,
+  },
+  {
+    id: "password",
+    type: "password",
+    label: "Mot de passe",
+    placeholder: "Entrez votre mot de passe",
+    guidance: (errors: any) =>
+      errors.password
+        ? {
+            type: "error",
+            message: "Le mot de passe doit contenir au moins 8 caractères",
+          }
+        : null,
+  },
+  /* todo add checkbox */
+]
 
-
-export const signUpInputsSchema = yup.object().shape({
+export const signUpInputsSchema = yup
+  .object()
+  .shape({
     email: yup.string().email().required(),
-    password: yup.string().min(8).required(),
-    passwordConfirmation: yup.string()
-    .oneOf([yup.ref('password'), null])
-}).required();
+    password: yup.string().min(2).required(),
+    passwordConfirmation: yup.string().oneOf([yup.ref("password"), null]),
+    firstName: yup.string().required(),
+    lastName: yup.string().required(),
+  })
+  .required()
 
 export const signUpInputs: Array<IFormInput> = [
-        {
-            id: 'email',
-            type: "email",
-            label: "Email",
-            placeholder: "Entrez votre email",
-            guidance: (errors: any) =>  errors.email ?  ({ type: "error", message: "L'email n'est pas valide" })  : null
-        },
-        {
-            id: 'password',
-            type: "password",
-            label: "Mot de passe",
-            placeholder: "Entrez votre mot de passe",
-            guidance: (errors: any) =>  errors.password ?  ({ type: "error", message: "Le mot de passe doit contenir au moins 8 caractères" })  : ({ type: "info", message: "Il ne doit contenir au minimum 8 caractères dont au moins une lettre majuscule, un chiffre et un caractère spécial." })
-        },
-        {
-            id: 'passwordConfirmation',
-            type: "password",
-            label: "Confirmation de mot de passe",
-            placeholder: "Confirmez votre mot de passe",
-            guidance: (errors: any) =>  errors.passwordConfirmation ? ({ type: "error", message: "Les mots de passe ne correspondent pas" }) : null 
-        }
-    ] 
+  {
+    id: "email",
+    type: "email",
+    label: "Email",
+    placeholder: "Entrez votre email",
+    guidance: (errors: any) =>
+      errors.email
+        ? { type: "error", message: "L'email n'est pas valide" }
+        : null,
+  },
+  {
+    id: "password",
+    type: "password",
+    label: "Mot de passe",
+    placeholder: "Entrez votre mot de passe",
+    guidance: (errors: any) =>
+      errors.password
+        ? {
+            type: "error",
+            message: "Le mot de passe doit contenir au moins 8 caractères",
+          }
+        : {
+            type: "info",
+            message:
+              "Il ne doit contenir au minimum 8 caractères dont au moins une lettre majuscule, un chiffre et un caractère spécial.",
+          },
+  },
+  {
+    id: "passwordConfirmation",
+    type: "password",
+    label: "Confirmation de mot de passe",
+    placeholder: "Confirmez votre mot de passe",
+    guidance: (errors: any) =>
+      errors.passwordConfirmation
+        ? { type: "error", message: "Les mots de passe ne correspondent pas" }
+        : null,
+  },
+  {
+    id: "firstName",
+    type: "text",
+    label: "Prénom",
+    placeholder: "Votre prénom",
+    guidance: () => null,
+  },
+  {
+    id: "lastName",
+    type: "text",
+    label: "Nom",
+    placeholder: "Votre nom",
+    guidance: () => null,
+  },
+]
--- pages/sign-up/index.tsx
+++ pages/sign-up/index.tsx
@@ -1,23 +1,48 @@
 import React from "react"
-import { Text, LoginForm, SplitScreen, Link, } from "@components"
+import { useRouter } from "next/router"
+import axios from "axios"
+import { Text, LoginForm, SplitScreen, Link } from "@components"
 import { signUpInputs, signUpInputsSchema } from "../../data/form"
 
-const Form = () => { 
+const Form = () => {
+  const router = useRouter()
+  const onSubmit = (data: any) => {
+    const formattedData = {
+      email: data.email,
+      password: data.password,
+      fisrtname: data.firstName,
+      lastname: data.lastName,
+    }
+    console.log(formattedData)
+    axios
+      .post(`${process.env.API_BASE_URL}/register`, data)
+      .then((res: { status: number; data: any }) => {
+        console.log(res.data)
+
+        // if (res.status === 200) {
+        //   router.push("/")
+        // }
+      })
+  }
+
   return (
-    <LoginForm inputs={signUpInputs} schema={signUpInputsSchema} onSubmit={() => { alert('submit') }} submitText="Je m'inscris" title="Inscription">
-      <Text  color='black' tag='div' typo='paragraph-md' >
+    <LoginForm
+      inputs={signUpInputs}
+      schema={signUpInputsSchema}
+      onSubmit={onSubmit}
+      submitText="Je m'inscris"
+      title="Inscription"
+    >
+      <Text color="black" tag="div" typo="paragraph-md">
         Vous avez déjà un compte ? &nbsp;
-        <Link to='/sign-in' label='Connectez-vous'/>
+        <Link to="/sign-in" label="Connectez-vous" />
       </Text>
     </LoginForm>
   )
 }
 
-
 const SignUp: React.FC = () => {
-    return (
-        <SplitScreen component={<Form />} image="./sign-up-image.png"/>
-    )
+  return <SplitScreen component={<Form />} image="./sign-up-image.png" />
 }
 
 export default SignUp
