e9e626a8d9f4506f0df9ea546d2c345bea55a78c
quentintrouve <trouve.quentin@gmail.com>
2022-07-05 13:44:55 +0200

    feat/ finish all filters

--- pages/works/index.tsx
+++ pages/works/index.tsx
@@ -10,99 +10,184 @@ enum scrollDirType  {
     down = "down"
 }
 
+type sortDateType = "asc" | "dsc"
+
+interface Filters {
+    search: string | null,
+    orderDate: sortDateType,
+    exhibitions: boolean
+}
+
 interface Work {
     id: string,
     title: string,
     description: string,
-    created_at: string
+    created_at: string,
+    exhibitions?: AttachedExhib[]
 }
 
-const data = [
+interface AttachedExhib {
+    title: string,
+    status: string
+}
+
+
+const data: Work[] = [
     {
         "id": "1",
         "title": "Ma mère, musicienne, est morte de maladie maligne à minuit, mardi à mercredi, au milieu du mois de mai mille977 au mouroir memor",
         "description": "Une description",
-        "created_at": "2022-06-30T23:09:10.693Z",
+        "created_at": "2022-06-27T23:09:10.693Z",
+        "exhibitions": [
+            {
+                "title": "Titre de l'exhibition",
+                "status": "validate"
+            },
+            {
+                "title": "Titre de l'exhibition 2",
+                "status": "pending"
+            }
+        ]
     },
     {
         "id": "2",
         "title": "Un titr",
         "description": "Une description",
-        "created_at": "2022-06-30T23:09:10.693Z",
+        "created_at": "2022-06-12T23:09:10.693Z",
+        "exhibitions": [
+            {
+                "title": "Titre de l'exhibition",
+                "status": "validate"
+            },
+        ]
     },
     {
         "id": "3",
         "title": "Un ttre",
         "description": "Une description",
-        "created_at": "2022-06-30T23:09:10.693Z",
+        "created_at": "2022-06-17T23:09:10.693Z",
     },
     {
         "id": "4",
         "title": "Un titre",
         "description": "Une description",
         "created_at": "2022-06-30T23:09:10.693Z",
+        "exhibitions": [
+            {
+                "title": "Titre de l'exhibition",
+                "status": "refused"
+            }
+        ]
+    },
+    {
+        "id": "5",
+        "title": "Un titre",
+        "description": "Une description",
+        "created_at": "2022-05-02T23:09:10.693Z",
     },
 ]
 
 const Works: React.FC = () => {
 
     const [works, setWorks] = useState<Work[]>([])
-    console.log(works)
-
-    const [filterExposed, setFilterExposed] = useState<boolean>(false)
-    const [filterUnexposed, setFilterUnexposed] = useState<boolean>(false)
-    const [searchValue, setSearchValue] = useState<string>("")
-    const [searchList, setSearchList] = useState<Work[]>([])
+    const [filters, setFilters] = useState<Filters>({
+        search: null,
+        orderDate: "dsc",
+        exhibitions: false,
+    })
 
     const [direction, setDirection] = useState<scrollDirType.up | scrollDirType.down>()
     const scrollDirection = useScrollDirection()
 
 
-    const onChangeCheckbox = (event) => {
-        
-        if(event.target.name === "works-exposed") {
-            setFilterExposed(prev => !prev)
-            filterUnexposed && setFilterUnexposed(false)
-        }
-
-        if(event.target.name === "works-unexposed") {
-            setFilterUnexposed(prev => !prev)
-            filterExposed && setFilterExposed(false)
-        }
+    const handleCheckExhibitions = () => {
+        setFilters(prev => {
+            return {
+                ...prev,
+                exhibitions: !filters.exhibitions
+            }
+        })
     }
 
     const handleSearch = (event) => {
         let normalizedValue = makeCaseAndAccentInsensitiveString(event.target.value)
-        setSearchValue(normalizedValue)
+        setFilters( prev => {
+            return {
+                ...prev,
+                search: normalizedValue === "" ? null : normalizedValue
+            }
+        })
     }
-    
-    const searchProcess = (value: string) => {
-        let search =  works.filter(work => {
-            let normalizedTitle = makeCaseAndAccentInsensitiveString(work.title)
-            return normalizedTitle.includes(searchValue)
+
+    const handleSortDate = () => {
+        filters.orderDate === "asc" && setFilters( prev => {
+            return {
+                ...prev,
+                orderDate: "dsc"
+            }
+        })
+
+        filters.orderDate === "dsc" && setFilters( prev => {
+            return {
+                ...prev,
+                orderDate: "asc"
+            }
+        })
+    }
+
+    const filterExhibitionsProcess = (list: Work[]) => {
+        let attachedExhibitions = list.filter(item => {
+            return item.exhibitions 
+        })
+
+        return attachedExhibitions
+    }
+
+    const searchProcess = (value: string, list: Work[]) => {
+        let search =  list.filter(item => {
+            let normalizedTitle = makeCaseAndAccentInsensitiveString(item.title)
+            return normalizedTitle.includes(value)
         })
 
         return search
     }
 
+    const sortDateProcess = (list: Work[]) => {
+        return list.sort((a, b) => {
+            let dateA: any = new Date(a.created_at)
+            let dateB: any = new Date(b.created_at)
+
+            if (filters.orderDate === "asc") return dateA - dateB
+            else if (filters.orderDate === "dsc") return dateB - dateA
+            else return 0
+        })
+    }
+
+    const filterWorksList = (list: Work[], filters) => {
+        console.log("fiiiltre")
+        let newList: Work[] = [...list]
+
+        if(filters.search !== null) {
+            newList = searchProcess(filters.search, list)
+        } 
+
+        if(filters.exhibitions) {
+            newList = filterExhibitionsProcess(newList)
+        }
+
+        if(filters.orderDate !== null) {
+            newList = sortDateProcess(newList)
+        }
+
+        return newList
+    }
+
     /* Init works */
     useEffect(() => {
+        console.log('use init')
         setWorks(data)
-        setSearchList(data)
     }, [])
     
-    /* Search process */
-    useEffect(() => {
-        console.log("ici")
-        if(searchValue) {
-            console.log("oui")
-            let newSearchList = searchProcess(searchValue)
-            setSearchList(newSearchList)
-        } else {
-            setSearchList(works)
-        }
-    }, [searchValue, works])
-
     /* Scroll Event */
     useEffect(() => {
         scrollDirection === "down" ?
@@ -120,21 +205,13 @@ const Works: React.FC = () => {
                     <ul className={style.filters}>
                         <li>
                             <Checkbox 
-                                checkboxLabel="Exposées"
+                                checkboxLabel="Assignée à une Exposition"
                                 checkboxName="works-exposed"
-                                isChecked={filterExposed}
-                                onChange={onChangeCheckbox}
+                                isChecked={filters.exhibitions}
+                                onChange={handleCheckExhibitions}
                             />
                         </li>
-                        <li className={style.filter}>
-                            <Checkbox 
-                                checkboxLabel="Non exposées"
-                                checkboxName="works-unexposed"
-                                isChecked={filterUnexposed}
-                                onChange={onChangeCheckbox}
-                            />
-                        </li>
-                        <li className={style.date}>
+                        <li className={style.date} onClick={() => handleSortDate()}>
                             <Icon type="downArrow" size="small" color="black" />
                             <Text tag="p" typo="label">Date de création</Text>
                         </li>
@@ -142,13 +219,13 @@ const Works: React.FC = () => {
                     <Search 
                         id="works-search" 
                         placeholder="Rechercher une oeuvre par son titre" 
-                        value={searchValue} 
+                        value={filters?.search ? filters.search : '' } 
                         onChange={handleSearch}
                     />
                 </aside>
             </section>
             <section className={style.bodySection}>
-                {searchList.map((work) => (
+                {filterWorksList(works, filters).map((work) => (
                     <CardGallery key={work.id} title={work.title} createdAt={work.created_at}/>
                 ))}
             </section>
