0fad43c24b45353725bcbed233fdaafdf6c08023
quentingrchr <51860690+quentingrchr@users.noreply.github.com>
2022-07-05 16:32:02 +0200

    add map input

--- pages/exhibitions/index.tsx
+++ pages/exhibitions/index.tsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React from "react"
 import { TemplatePage, Text } from "@components"
 
 const Exhibitions: React.FC = () => {
-    return (
-        <TemplatePage isLogged={true}>
-            <Text tag="h1" typo="heading-lg">Expositions</Text>
-        </TemplatePage>
-    )
+  return (
+    <TemplatePage isLogged={true}>
+      <Text tag="h1" typo="heading-lg">
+        Expositions
+      </Text>
+    </TemplatePage>
+  )
 }
 
-export default Exhibitions;
+export default Exhibitions
--- src/components/map/index.tsx
+++ src/components/map/index.tsx
@@ -1,34 +1,117 @@
 import React, { useRef, useEffect, useState } from "react"
-import { Map as MapGl } from "react-map-gl"
+import {
+  Map as MapGl,
+  Source,
+  Layer,
+  FeatureCollection,
+  Marker,
+  NavigationControl,
+} from "react-map-gl"
+import { useController, useFormContext } from "react-hook-form"
 import styles from "./index.module.scss"
 import "mapbox-gl/dist/mapbox-gl.css"
 
+const INITIAL_STATE = {
+  longitude: -2.3522219,
+  latitude: 48.856614,
+  zoom: 3.5,
+}
+
+interface IInitialVP {
+  longitude: number
+  latitude: number
+  zoom: number
+}
+interface IGalleryMap {
+  id: string
+  lat: number
+  lng: number
+  price: number
+  name: string
+}
+
+interface IProps {
+  name: string
+  required?: boolean
+  galleries: IGalleryMap[]
+  initialViewport?: IInitialVP
+}
+
 const accessToken =
-  "pk.eyJ1IjoicXVlbnRpbmdyY2hyIiwiYSI6ImNsNTZycHdjeTA2Y2czaGxmeGVyYzh0cDMifQ.jkEQnKy3I1Rd7cRyEjlfeg"
-
-const accessToken2 =
   "pk.eyJ1IjoicXVlbnRpbmdyY2hyIiwiYSI6ImNsNTZyanNzcjA4dDIzZG5zbHV1MDA1bHQifQ.TlP4iPDrNLXYKTYIZVEOPg"
-export type IProps = {}
 
-export const Map: React.FC<IProps> = (props: IProps) => {
+const dummyData: IGalleryMap[] = [
+  {
+    id: "1",
+    lat: 48.852614,
+    lng: 2.3522219,
+    price: 100,
+    name: "Paris",
+  },
+  {
+    id: "2",
+    lat: 48.856614,
+    lng: 2.3521219,
+    price: 90,
+    name: "Paris",
+  },
+  {
+    id: "3",
+    lat: 48.956614,
+    lng: 2.3622219,
+    price: 110,
+    name: "Paris",
+  },
+]
+
+export const Map: React.FC<IProps> = ({
+  name = "board_id",
+  galleries = dummyData,
+  initialViewport = INITIAL_STATE,
+  required = false,
+}: IProps) => {
   const mapRef = useRef(null)
-  const map = useRef(null)
-  const [lng, setLng] = useState(-70.9)
-  const [lat, setLat] = useState(42.35)
-  const [zoom, setZoom] = useState(9)
+  const [viewport, setViewport] = useState(initialViewport)
+  const { control } = useFormContext() // retrieve all hook methods
+  const {
+    field: { onChange, onBlur, value, ref },
+    // fieldState: { invalid, isTouched, isDirty },
+    // formState: { touchedFields, dirtyFields }
+  } = useController({
+    name,
+    control,
+    rules: { required: required },
+    defaultValue: "",
+  })
 
-  useEffect(() => {}, [])
+  const handleChange = (boardId: string) => {
+    onChange(boardId)
+  }
 
   return (
     <div>
       <MapGl
         ref={mapRef}
-        mapboxAccessToken={accessToken2}
+        mapboxAccessToken={accessToken}
         style={{ width: 600, height: 600 * 0.8 }}
+        {...viewport}
         mapStyle="mapbox://styles/quentingrchr/cl56uzn94000314o2perf5dxt"
-        latitude={lat}
-        longitude={lng}
-      />
+        onMove={(evt) => setViewport(evt.viewState)}
+      >
+        <NavigationControl />
+        {galleries.map((item: IGalleryMap) => {
+          return (
+            <Marker longitude={item.lng} latitude={item.lat} anchor="bottom">
+              <div
+                className={styles.marker}
+                onClick={() => handleChange(item.id)}
+              >
+                <p>{item.price} € / j</p>
+              </div>
+            </Marker>
+          )
+        })}
+      </MapGl>
     </div>
   )
 }
