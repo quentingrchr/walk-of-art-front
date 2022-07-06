2c1a1ecdfedf0f98a04ef4352609344dbf5a10fb
quentingrchr <51860690+quentingrchr@users.noreply.github.com>
2022-07-06 17:57:19 +0200

    wip

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,32 @@
 import React from "react"
 import { Cards, TemplatePage, Text } from "@components"
-
+import { useRecoilValue } from "recoil"
+import { userState } from "@recoil/user/atom"
 const Home: React.FC = () => {
-  return (
-    <TemplatePage>
-      <Text tag="h1" typo="paragraph-md">
-        Page de présentation
-      </Text>
-      <Cards title="Titre de l’expo qui peut etre sur deux lignes" />
-    </TemplatePage>
-  )
+  const user = useRecoilValue(userState)
+
+  const AnonymousHome = () => {
+    return <div>Bonjour personne anonyme</div>
+  }
+  const ArtistHome = (user) => {
+    return <div>Bonjour {user.email}, voici votre dashboard</div>
+  }
+  const ModeratorHome = () => {
+    return <div>Bonjour modérateur</div>
+  }
+
+  const roleHomeComponentMap = {
+    ROLE_USER: <ArtistHome user={user} />,
+    ROLE_MODERATOR: <ModeratorHome />,
+  }
+
+  if (user !== null) {
+    // user is logged in
+    return <TemplatePage>{roleHomeComponentMap[user.role]}</TemplatePage>
+  } else {
+    // no user logged
+    return <TemplatePage>{<AnonymousHome />}</TemplatePage>
+  }
 }
 
 export default Home
--- pages/sign-in/index.tsx
+++ pages/sign-in/index.tsx
@@ -1,7 +1,12 @@
 import React from "react"
 import { useRouter } from "next/router"
+import jwt from "jsonwebtoken"
+import axios from "axios"
+import { BASE_API_URL } from "@const/index"
 
 import { Text, LoginForm, SplitScreen, Link } from "@components"
+import { useRecoilValue, useSetRecoilState } from "recoil"
+import { userState } from "@recoil/user/atom"
 import { signInInputs, signInInputsSchema } from "../../data/form"
 import { axiosInstance } from "@utility/index"
 import {
@@ -19,11 +24,14 @@ interface ILoginRequest {
 
 const Form = () => {
   const router = useRouter()
+  const setUser = useSetRecoilState(userState)
 
   const login = async (params: ILoginRequest) => {
-    const response = await axiosInstance.post("/login_check", params)
+    const response = await axios.post(`${BASE_API_URL}/login_check`, params)
     const { status, data } = response
     if (status === 200) {
+      console.log(response.data.token)
+      setUser(jwt.decode(response.data.token))
       setAuthTokens({
         accessToken: response.data.token,
         refreshToken: response.data.refresh_token,
