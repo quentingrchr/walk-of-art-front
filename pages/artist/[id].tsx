ca68417cb316d1475a7c60a7e5bc844b5553167d
Raphael <raphael.ferreira@hetic.net>
2022-07-07 15:21:43 +0200

    add links part

--- pages/artist/[id].tsx
+++ pages/artist/[id].tsx
@@ -4,7 +4,7 @@ import s from "./index.module.scss"
 import { Logo, Text, ImagesPreview, Button } from '@components'
 
 const data = {
-
+    name: 'Fabien Deneau'
 }
 
 const Artist: React.FC = () => {
@@ -27,7 +27,7 @@ const Artist: React.FC = () => {
                 <div className={s.description}>
                     <Text tag="p" typo="paragraph-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In aliquet dictumst sed aliquet nulla sed. Arcu at sagittis a placerat. Aenean cum ut dolor platea diam. Aliquam mi ac dictum tempor eget dictum tristique imperdiet. Tristique consectetur vitae mi amet, adipiscing quis vitae ac. Nunc sit eu elementum cursus. Sagittis lectus eu turpis et adipiscing tempor quis id egestas. Ac mi nibh at interdum id turpis. Interdum sapien purus quis id varius molestie tristique sed.</Text>
                     <div className={s.description__name}>
-                        <Text tag="p" typo="paragraph-md">Fabien Deneau</Text>
+                        <Text tag="p" typo="paragraph-md">{data.name}</Text>
                     </div>
                 </div>
                 <div className={s.reaction}>
@@ -39,6 +39,29 @@ const Artist: React.FC = () => {
                         target="_blank"
                     />
                 </div>
+                <Text tag="h3" typo="heading-xs">Réseaux sociaux de<br/>{data.name}</Text>
+                <Text tag="h3" typo="heading-xs">Liens de {data.name}</Text>
+                <Button
+                    label={'Site personnel'}
+                    to="https://www.google.com"
+                    bg="light"
+                    color="black"
+                    target="_blank"
+                />
+                <Button
+                    label={'Portfolio'}
+                    to="https://www.google.com"
+                    bg="light"
+                    color="black"
+                    target="_blank"
+                />
+                <Button
+                    label={'Boutique en ligne'}
+                    to="https://www.google.com"
+                    bg="light"
+                    color="black"
+                    target="_blank"
+                />
             </section>
         </>
     )
