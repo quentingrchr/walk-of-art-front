39a9c5081dbf1a6e57306b2b6a82547c898d1ccc
Raphael <raphael.ferreira@hetic.net>
2022-07-07 14:33:17 +0200

    add main content

--- pages/artist/[id].tsx
+++ pages/artist/[id].tsx
@@ -1,14 +1,36 @@
 import React, { useState, useEffect } from "react"
 import { useRouter } from "next/router"
-import style from "./index.module.scss"
-import { Text } from '@components'
+import s from "./index.module.scss"
+import { Logo, Text, ImagesPreview } from '@components'
+
+const data = {
+
+}
 
 const Artist: React.FC = () => {
     const router = useRouter()
+    const { id } = router.query
 
     return (
         <>
-            <Text tag="p" typo="paragraph-md">Hello World</Text>
+            <section className={s.artist}>
+                <Logo to="/" />
+                <ImagesPreview
+                    primaryImage="https://iili.io/FhDd9R.jpg"
+                    secondaryImages={[
+                        'https://iili.io/A7NDAP.jpg',
+                        'https://iili.io/dwagF2.png',
+                        'https://iili.io/hy8bLv.jpg'
+                    ]}
+                />
+                <Text tag="h2" typo="heading-md">Voila le titre de l'exposition que je visite</Text>
+                <div className={s.description}>
+                    <Text tag="p" typo="paragraph-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In aliquet dictumst sed aliquet nulla sed. Arcu at sagittis a placerat. Aenean cum ut dolor platea diam. Aliquam mi ac dictum tempor eget dictum tristique imperdiet. Tristique consectetur vitae mi amet, adipiscing quis vitae ac. Nunc sit eu elementum cursus. Sagittis lectus eu turpis et adipiscing tempor quis id egestas. Ac mi nibh at interdum id turpis. Interdum sapien purus quis id varius molestie tristique sed.</Text>
+                    <div className={s.description__name}>
+                        <Text tag="p" typo="paragraph-md">Fabien Deneau</Text>
+                    </div>
+                </div>
+            </section>
         </>
     )
 }
