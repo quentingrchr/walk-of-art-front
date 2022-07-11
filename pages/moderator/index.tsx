edac0db7d2415518299c79ecc2e3bac12fa1ac84
quentingrchr <51860690+quentingrchr@users.noreply.github.com>
2022-07-11 11:14:48 +0200

    add error handling in login form

---
+++ pages/moderator/index.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+import { useRouter } from "next/router"
+
+const Moderator: React.FC = () => {
+  return <div>Moderator home</div>
+}
+
+export default Moderator
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,14 +2,19 @@ import React, { useState } from "react"
 import { isLoggedIn, clearAuthTokens } from "axios-jwt"
 import styles from "./index.module.scss"
 import Link from "next/link"
+import { useRecoilValue } from "recoil"
 import { Logo, Navbar, DropdownButton, Icon, Button } from "@components"
 import { useRouter } from "next/router"
 import { NotificationWrapper } from "@components/notification-wrapper"
 import { eraseCookie } from "@utility/index"
+import { userState } from "@recoil/user/atom"
+import { UserRolesType } from "../../types"
 
-export type IProps = {}
+interface IProps {}
 
 export const Header: React.FC<IProps> = (props: IProps) => {
+  const user = useRecoilValue(userState)
+  console.log({ user }, "nav")
   const [appear, setAppear] = useState(false)
   const notifs = [
     {
