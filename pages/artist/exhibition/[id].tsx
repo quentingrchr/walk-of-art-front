854a99829151dd2e095dd54e5eb16b2172fdcd29
quentintrouve <trouve.quentin@gmail.com>
2022-07-11 12:25:48 +0200

    fix

--- pages/artist/exhibition/[id].tsx
+++ pages/artist/exhibition/[id].tsx
@@ -16,9 +16,9 @@ import {
   Unauthorized,
   ButtonArrow
 } from "@components"
-import cardImg from "../../src/assets/images/cardImg.png"
-import { getDateWithoutHours, windowIsNotReady } from "../../src/utility"
-import { Exhibition } from "../../src/types"
+import cardImg from "../../../src/assets/images/cardImg.png"
+import { getDateWithoutHours, windowIsNotReady } from "../../../src/utility"
+import { Exhibition } from "../../../src/types"
 
 
 const data: Exhibition = {
