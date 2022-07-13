fe4e2ece1442b787c54ecd279f97e18cb118340d
Raphael <raphael.ferreira@hetic.net>
2022-07-13 12:12:46 +0200

    create element on click

--- pages/gallery/[id].tsx
+++ pages/gallery/[id].tsx
@@ -1,16 +1,36 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect, ReactElement } from "react"
 import { useRouter } from "next/router"
 import { useForm } from "react-hook-form"
 import s from "./index.module.scss"
+import { Icons } from "@interfaces/index"
+import { Icon } from "@components"
+
+interface IEmoji {
+    type: Icons,
+    offset: number
+}
 
 const Artist: React.FC = () => {
     const router = useRouter()
     const { id } = router.query
+    const [emojiList, setEmojiList] = useState<IEmoji[]>([])
+
+    const createNewEmoji = (type: Icons, offset: number): void =>
+    {
+        setEmojiList(prev => [...prev, { type, offset }])
+        // console.log(emojiList)
+    }
 
     return (
         <>
-            <section className={s.gallery}>
-                <div className={s.emoji}></div>
+            <section className={s.gallery} onClick={() => createNewEmoji('smiley', Math.random() * 80)}>
+                {
+                    emojiList.map((element, index) =>
+                    (
+                        <div className={s.emoji} style={{left: `${element.offset}%`}} key={index}>
+                        </div>
+                    ))
+                }
             </section>
         </>
     )
