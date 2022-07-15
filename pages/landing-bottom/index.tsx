0bb9e024658ec943e3bfb44ca21f72063d464dbc
gRAKLECLER <guillaume.raklecler@meero.com>
2022-07-16 01:57:56 +0200

    finish bottom part of landing

--- pages/landing-bottom/index.tsx
+++ pages/landing-bottom/index.tsx
@@ -1,61 +1,110 @@
 import React from "react"
-import {Logo, Text, HeadingStrong, InputGroup, Button} from "@components"
+import {Logo, Text, HeadingStrong, InputGroup, Button, Stepper} from "@components"
 import { useForm } from "react-hook-form";
+import mock from "../../src/assets/images/mock.png"
 import landingGallery from "../../src/assets/images/landingGallery.png"
+import landingGallery2 from "../../src/assets/images/landingGallery2.png"
+import landingGallery3 from "../../src/assets/images/landingGallery3.png"
+import landingGallery4 from "../../src/assets/images/landingGallery4.png"
+import landingGallery5 from "../../src/assets/images/landingGallery5.png"
+import qrCode from "../../src/assets/images/qrCode.png"
+import visiteur from "../../src/assets/images/visiteur.png"
 import s from "./index.module.scss"
-import { ImageCard } from "@components/image-card";
+import { Step } from "@components/stepper/step";
 
 
 const LandingBottom: React.FC = () => {
+    const STEPS = [
+        {
+          id: 1,
+          label: "Le visiteur passe devant le panneau",
+          number: 1,
+          completed: true,
+          img: mock.src
+        },
+        {
+          id: 2,
+          label: "Le visiteur scanne le QR codens",
+          number: 2,
+          completed: true,
+          img: qrCode.src
+        },
+        {
+          id: 3,
+          label: "le visiteur bénéficie d’informations complémentaires",
+          number: 3,
+          completed: true,
+          img: visiteur.src
+        },
+      ]
     const { register, handleSubmit, watch } = useForm({
         mode: "onBlur",
-        // defaultValues,
       });
     return (
         <>
-        {/* <ImageCard src={landingGallery} alt={landingGallery} orientation='portrait'/> */}
-        <div className={s.landing__gallery}>
+        <section className={s.landing__guidance}>
+                <HeadingStrong elementColor="violet" content="Le visiteur guidé par sa curiosité"/>
+                <div className={s.landing__containerStepper}>
+                        {STEPS.map((step, index) => (
+                            <div className={s.landing__contentGuidance}>
+                                <div className={s.landing__step}>
+                                    <Step
+                                        label={step.label}
+                                        number={step.number}
+                                        completed={false}
+                                        active={true}
+                                        key={index}
+                                        disable={false}
+                                        variant={undefined}
+                                    />
+                                </div>
+                                <img src={step.img} alt={step.img} />
+                            </div>
+                        ))}
+                </div>
+        </section>
+        <section className={s.landing__gallery}>
                 <HeadingStrong elementColor="success-light" content="Galerie des oeuvres exposées"/>
                 <div className={s.landing__galleryContainer}>
                     <picture className={s.landing__galleryContent}>
-                        <img className={s.landing__galleryImg} src="/_next/static/media/landingGallery.88360cb8.png" alt="" />
+                        <img className={s.landing__galleryImg} src={landingGallery.src} alt="" />
                     </picture>
                     <picture className={s.landing__galleryContent}>
-                        <img className={s.landing__galleryImg} src="/_next/static/media/landingGallery.88360cb8.png" alt="" />
+                        <img className={s.landing__galleryImg} src={landingGallery3.src} alt="" />
                     </picture>
                     <picture className={s.landing__galleryContent}>
-                        <img className={s.landing__galleryImg} src="/_next/static/media/landingGallery.88360cb8.png" alt="" />
+                        <img className={s.landing__galleryImg} src={landingGallery2.src} alt="" />
                     </picture>
                     <picture className={s.landing__galleryContent}>
-                        <img className={s.landing__galleryImg} src="/_next/static/media/landingGallery.88360cb8.png" alt="" />
+                        <img className={s.landing__galleryImg} src={landingGallery3.src} alt="" />
                     </picture>
                     <picture className={s.landing__galleryContent}>
-                        <img className={s.landing__galleryImg} src="/_next/static/media/landingGallery.88360cb8.png" alt="" />
+                        <img className={s.landing__galleryImg} src={landingGallery2.src} alt="" />
                     </picture>
                     <picture className={s.landing__galleryContent}>
-                        <img className={s.landing__galleryImg} src="/_next/static/media/landingGallery.88360cb8.png" alt="" />
+                        <img className={s.landing__galleryImg} src={landingGallery5.src} alt="" />
                     </picture>
                     <picture className={s.landing__galleryContent}>
-                        <img className={s.landing__galleryImg} src="/_next/static/media/landingGallery.88360cb8.png" alt="" />
+                        <img className={s.landing__galleryImg} src={landingGallery4.src} alt="" />
                     </picture>
                     <picture className={s.landing__galleryContent}>
-                        <img className={s.landing__galleryImg} src="/_next/static/media/landingGallery.88360cb8.png" alt="" />
+                        <img className={s.landing__galleryImg} src={landingGallery3.src} alt="" />
                     </picture>
                     <picture className={s.landing__galleryContent}>
-                        <img className={s.landing__galleryImg} src="/_next/static/media/landingGallery.88360cb8.png" alt="" />
+                        <img className={s.landing__galleryImg} src={landingGallery5.src} alt="" />
                     </picture>
                     <picture className={s.landing__galleryContent}>
-                        <img className={s.landing__galleryImg} src="/_next/static/media/landingGallery.88360cb8.png" alt="" />
+                        <img className={s.landing__galleryImg} src={landingGallery2.src} alt="" />
                     </picture>
                     <picture className={s.landing__galleryContent}>
-                        <img className={s.landing__galleryImg} src="/_next/static/media/landingGallery.88360cb8.png" alt="" />
+                        <img className={s.landing__galleryImg} src={landingGallery4.src} alt="" />
                     </picture>
                     <picture className={s.landing__galleryContent}>
-                        <img className={s.landing__galleryImg} src="/_next/static/media/landingGallery.88360cb8.png" alt="" />
+                        <img className={s.landing__galleryImg} src={landingGallery.src} alt="" />
                     </picture>
                 </div>
-        </div>
-        <div className={s.landing__contact}>
+        </section>
+        <section className={s.landing__contact}>
                 <HeadingStrong elementColor="success-light" content="Nous sommes à votre disposition"/>
                     <Text tag="p" typo="paragraph-sm">Si vous souhaitez avoir d‘autres informations, n’hésitez pas à nous contacter !
     Nous tâcherons de vous répondre dans les meilleurs délais</Text>
@@ -78,7 +127,7 @@ const LandingBottom: React.FC = () => {
                 />
                 <Button label={"Envoyer"} color="white" bg="dark" type="submit" />
             </form>
-            </div>
+            </section>
             <footer className={s.landing__footer}>
                 <div className={s.landing__footerLogo}>
                     <Logo to={'/'} color="white" />
