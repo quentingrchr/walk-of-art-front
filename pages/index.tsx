c9a465a60acc4875210a217ea77f9d3f9bd0bd81
quentintrouve <trouve.quentin@gmail.com>
2022-07-17 03:10:55 +0200

    feat/finish first section and mix avec section coded by guillaume

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,8 @@ import { useRouter } from "next/router"
 import { userState } from "@recoil/user/atom"
 import { windowIsNotReady } from "../src/utility"
 import { isLoggedIn } from "axios-jwt"
-import { Landing } from "@components"
+import { Landing, Logo, Text  } from "@components"
+import style from '../src/components/landing/index.module.scss'
 
 
 const Home: React.FC = () => {
@@ -22,9 +23,22 @@ const Home: React.FC = () => {
   }
 
   return (
+    <>
     <TemplatePage>
       <Landing />
     </TemplatePage>
+    <footer className={style.landing__footer}>
+      <div className={style.landing__footerLogo}>
+          <Logo to={'/'} color="white" />
+          <Text tag="p" typo="paragraph-md-bold">© 2022</Text>
+      </div>
+      <div className={style.landing__footerLinks}>
+          <li>Politique de confidentalité</li>
+          <li>Conditions d’utilisation</li>
+          <li>Nous rejoindre</li>
+      </div>
+    </footer>
+    </>
   )
 }
 
--- src/components/landing/index.tsx
+++ src/components/landing/index.tsx
@@ -1,6 +1,8 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import style from "./index.module.scss"
-import { HeadingStrong, Text, Button, ButtonArrow, Icon } from "@components"
+import { useForm } from "react-hook-form";
+import { Logo, HeadingStrong, Text, Button, ButtonArrow, Icon, InputGroup } from "@components"
+import { Step } from "@components/stepper/step";
 
 // Images
 import Landing1 from '../../assets/images/landing-1.png'
@@ -8,11 +10,81 @@ import Landing2 from '../../assets/images/landing-2.png'
 import Landing3 from '../../assets/images/landing-3.png'
 import Landing4 from '../../assets/images/landing-4.png'
 import Landing5 from '../../assets/images/landing-5.png'
+import Landing6 from '../../assets/images/landing-6.png'
+import Landing7 from '../../assets/images/landing-7.png'
+import Landing8 from '../../assets/images/landing-8.png'
+import mock from "../../assets/images/mock.png"
+import landingGallery from "../../assets/images/landingGallery.png"
+import landingGallery2 from "../../assets/images/landingGallery2.png"
+import landingGallery3 from "../../assets/images/landingGallery3.png"
+import landingGallery4 from "../../assets/images/landingGallery4.png"
+import landingGallery5 from "../../assets/images/landingGallery5.png"
+import qrCode from "../../assets/images/qrCode.png"
+import visitor from "../../assets/images/visiteur.png"
+
+enum ArtistDetails {
+    work = "work",
+    exhib = "exhib",
+    display = "display"
+}
+
 
 export const Landing: React.FC = () => {
+
+    const [artistDetails, setArtistDetails] = useState<ArtistDetails | null>(null)
+
+    const visitorSteps = [
+        {
+            id: 1,
+            label: "Le visiteur passe devant le panneau",
+            number: 1,
+            completed: true,
+            img: mock.src
+        },
+        {
+            id: 2,
+            label: "Le visiteur scanne le QR codens",
+            number: 2,
+            completed: true,
+            img: qrCode.src
+        },
+        {
+            id: 3,
+            label: "le visiteur bénéficie d’informations complémentaires",
+            number: 3,
+            completed: true,
+            img: visitor.src
+        },
+    ]
+
+    const getArtistImage = () => {
+        switch(artistDetails) {
+            case ArtistDetails.work:
+                return Landing6.src
+            case ArtistDetails.exhib:
+                return Landing7.src
+            case ArtistDetails.display:
+                return Landing8.src
+            default:
+                return Landing6.src 
+        }
+    }
+
+    useEffect(() => {
+        let details = document.querySelectorAll("details")
+
+        artistDetails && details.forEach(detailsElement => {
+            if(detailsElement.id !== artistDetails) {
+                detailsElement.removeAttribute('open')
+            }
+        })
+    }, [artistDetails])
+
+    const { register } = useForm()
+    
     return (
         <main className={style.landing}>
-            <section className={style.firstSection}>
+            <section className={style.landing__resume}>
                 <div className={style.bloc}>
                     <HeadingStrong elementColor="yellow" size="xxl" content="Partagez votre art avec le monde qui vous entoure." />
                     <Text tag="p" typo="paragraph-lg">
@@ -56,7 +128,7 @@ export const Landing: React.FC = () => {
                 </div>
             </section>
 
-            <section className={style.secondSection}>
+            <section className={style.landing__streetBanner}>
                 <HeadingStrong elementColor="violet" size="xl" content="Amenez vos expositions" />
                 <Text tag="h2" typo="heading-xl" color="white">
                     dans la rue.
@@ -64,7 +136,7 @@ export const Landing: React.FC = () => {
                 <img src={Landing4.src} />
             </section>
 
-            <section className={style.thirdSection}>
+            <section className={style.landing__screens}>
                 <div className={style.bloc}>
                     <HeadingStrong elementColor="yellow" size="xl" content="Des écrans haute définition pour afficher vos oeuvres." />
                     <Text tag="p" typo="paragraph-lg">
@@ -79,42 +151,137 @@ export const Landing: React.FC = () => {
                 </div>
             </section>
 
-            <section className={style.fourthSection}>
+            <section className={style.landing__artist}>
                 <HeadingStrong elementColor="pink" size="xl" content="Comment exposer ?" />
                 <div className={style.content}>
-                    <div className={style.mockup}><span>ok</span></div>
+                    <div className={style.mockup}>
+                        <img src={getArtistImage()} />
+                    </div>
                     <div className={style.steps}>
-                        <ul>
-                            <li className={style.step}>
-                                <Icon type="upload" size="xlarge" color="black"/>
-                                <div>
-                                    <h5>1. Uploadez les oeuvres</h5>
-                                    <Text tag="p" typo="paragraph-md">
-                                        Uploadez les fichiers correspondants à votre oeuvre au nombre de 4 maximum, choisissez le média principal qu’on verra en premier.
-                                    </Text>
-                                </div>
-                            </li>
-                            <li className={style.step}>
-                                <Icon type="calendar-landing" size="xlarge" color="black"/>
-                                <div>
-                                    <h5>2. Programmez une exposition</h5>
-                                    <Text tag="p" typo="paragraph-md">
-                                        Choisissez une oeuvre parmi la bibliothèque des oeuvres que vous aurez upload, choisissez où et quand vous voulez l’exposer.
-                                    </Text>
-                                </div>
-                            </li>
-                            <li className={style.step}>
-                                <Icon type="screen" size="xlarge" color="black"/>
-                                <div>
-                                    <h5>3. Ton exposition est affichée</h5>
-                                    <Text tag="p" typo="paragraph-md">
-                                        À la date que vous avez choisi, l’oeuvre sera affichée sur le panneau que vous aurez choisi pendant la durée programmée.
-                                    </Text>
-                                </div>
-                            </li>
-                        </ul>
+                        <div className={style.stepsContainer}>
+                            <details className={style.step} id={ArtistDetails.work} onClick={() => setArtistDetails(ArtistDetails.work)}>
+                                <summary>
+                                    <Icon type="upload" size="xlarge" color={artistDetails && artistDetails === ArtistDetails.work ? 'black' : 'grey-500'} />
+                                    <span>1. Uploadez les oeuvres</span>
+                                </summary>
+                                <Text tag="p" typo="paragraph-md">
+                                    Uploadez les fichiers correspondants à votre oeuvre au nombre de 4 maximum, choisissez le média principal qu’on verra en premier.
+                                </Text>
+                            </details>
+                            <details className={style.step} id={ArtistDetails.exhib} onClick={() => setArtistDetails(ArtistDetails.exhib)}>
+                                <summary>
+                                    <Icon type="calendar-landing" size="xlarge" color={artistDetails && artistDetails === ArtistDetails.exhib ? 'black' : 'grey-500'}/>
+                                    <span>2. Programmez une exposition</span>
+                                </summary>
+                                <Text tag="p" typo="paragraph-md">
+                                    Choisissez une oeuvre parmi la bibliothèque des oeuvres que vous aurez upload, choisissez où et quand vous voulez l’exposer.
+                                </Text>
+                            </details>
+                            <details className={style.step} id={ArtistDetails.display} onClick={() => setArtistDetails(ArtistDetails.display)}>
+                                <summary>
+                                    <Icon type="screen" size="xlarge" color={artistDetails && artistDetails === ArtistDetails.display ? 'black' : 'grey-500'}/>
+                                    <span>3. Ton exposition est affichée</span>
+                                </summary>
+                                <Text tag="p" typo="paragraph-md">
+                                    À la date que vous avez choisi, l’oeuvre sera affichée sur le panneau que vous aurez choisi pendant la durée programmée.
+                                </Text>
+                            </details>
+                        </div>
                     </div>
                 </div>
+            </section>
+
+            <section className={style.landing__guidance}>
+                <HeadingStrong elementColor="violet" content="Le visiteur guidé par sa curiosité."/>
+                <div className={style.landing__containerStepper}>
+                    {visitorSteps.map((step, index) => (
+                        <div className={style.landing__contentGuidance}>
+                            <div className={style.landing__step}>
+                                <Step
+                                    label={step.label}
+                                    number={step.number}
+                                    completed={false}
+                                    active={true}
+                                    key={index}
+                                    disable={false}
+                                    variant={undefined}
+                                />
+                            </div>
+                            <img src={step.img} alt={step.img} />
+                        </div>
+                    ))}
+                </div>
+            </section>
+
+            <section className={style.landing__gallery}>
+                <HeadingStrong elementColor="success-light" content="Galerie des oeuvres exposées."/>
+                <div className={style.landing__galleryContainer}>
+                    <picture className={style.landing__galleryContent}>
+                        <img className={style.landing__galleryImg} src={landingGallery.src} alt="" />
+                    </picture>
+                    <picture className={style.landing__galleryContent}>
+                        <img className={style.landing__galleryImg} src={landingGallery3.src} alt="" />
+                    </picture>
+                    <picture className={style.landing__galleryContent}>
+                        <img className={style.landing__galleryImg} src={landingGallery2.src} alt="" />
+                    </picture>
+                    <picture className={style.landing__galleryContent}>
+                        <img className={style.landing__galleryImg} src={landingGallery3.src} alt="" />
+                    </picture>
+                    <picture className={style.landing__galleryContent}>
+                        <img className={style.landing__galleryImg} src={landingGallery2.src} alt="" />
+                    </picture>
+                    <picture className={style.landing__galleryContent}>
+                        <img className={style.landing__galleryImg} src={landingGallery5.src} alt="" />
+                    </picture>
+                    <picture className={style.landing__galleryContent}>
+                        <img className={style.landing__galleryImg} src={landingGallery4.src} alt="" />
+                    </picture>
+                    <picture className={style.landing__galleryContent}>
+                        <img className={style.landing__galleryImg} src={landingGallery3.src} alt="" />
+                    </picture>
+                    <picture className={style.landing__galleryContent}>
+                        <img className={style.landing__galleryImg} src={landingGallery5.src} alt="" />
+                    </picture>
+                    <picture className={style.landing__galleryContent}>
+                        <img className={style.landing__galleryImg} src={landingGallery2.src} alt="" />
+                    </picture>
+                    <picture className={style.landing__galleryContent}>
+                        <img className={style.landing__galleryImg} src={landingGallery4.src} alt="" />
+                    </picture>
+                    <picture className={style.landing__galleryContent}>
+                        <img className={style.landing__galleryImg} src={landingGallery.src} alt="" />
+                    </picture>
+                </div>
+            </section>
+            <section className={style.landing__contact}>
+                <HeadingStrong elementColor="success-light" content="Nous sommes à votre disposition"/>
+                <Text tag="p" typo="paragraph-sm">
+                    Si vous souhaitez avoir d‘autres informations, n’hésitez pas à nous contacter !
+                    
+                </Text>
+                <Text tag="p" typo="paragraph-sm">
+                    Nous tâcherons de vous répondre dans les meilleurs délais
+                </Text>
+                <form className={style.landing__formContact}>
+                    <InputGroup
+                    placeholder="example@xyz.com"
+                    register={register}
+                    id="description"
+                    type="email"
+                    label="Votre mail"
+                    guidance={null}
+                    />
+                    <InputGroup
+                    placeholder="Rédigez ici votre demande"
+                    register={register}
+                    id="description"
+                    type="textarea"
+                    label="Votre demande"
+                    guidance={null}
+                    />
+                    <Button label={"Envoyer"} color="white" bg="dark" type="submit" />
+                </form>
             </section>
         </main>
     )
